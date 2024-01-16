476cd8db00c40857fc3b62ed59975c5f42bd815e
robmyers <me.robmyers@gmail.com>
2024-01-16 16:08:50 +0000

    feat: add mdx and check it works

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,29 @@
 import type { GatsbyConfig } from "gatsby";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `NPC CLI`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-emotion"]
+  plugins: [
+    "gatsby-plugin-emotion",
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `src/pages`,
+      },
+    },
+  ],
 };
 
 export default config;
