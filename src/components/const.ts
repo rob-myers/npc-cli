4df0496597175e9c2f1a387401c9bd8a5eba9be7
Rob Myers <me.robmyers@gmail.com>
2024-03-03 10:22:16 +0000

    feat: narrow Nav

--- src/components/const.ts
+++ src/components/const.ts
@@ -6,8 +6,8 @@ export const discussionsUrl = "https://github.com/rob-myers/npc-cli/discussions"
 export const nav = /** @type {const} */ {
   collapsedRem: 4,
   collapsedWidth: "4rem",
-  expandedRem: 14,
-  expandedWidth: "14rem",
+  expandedRem: 12,
+  expandedWidth: "12rem",
 };
 
 export const view = /** @type {const} */ {
