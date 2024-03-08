ef908d6d3b5a1103ed2c089909f29b363610dedd
robmyers <me.robmyers@gmail.com>
2024-03-08 17:43:17 +0000

    feat: clean

--- src/npc-cli/aux/infinite-grid-helper.js
+++ src/npc-cli/aux/infinite-grid-helper.js
@@ -14,7 +14,7 @@ export default class InfiniteGridHelper extends THREE.Mesh {
    * @param {string} [axes]
    */
   constructor(size1 = 10, size2 = 10, color, distance = 200, axes = "xyz") {
-    const planeAxes = axes.slice(0, 2);
+    const planeAxes = axes.slice(0, 2); // e.g. 'xy'
 
     const geometry = new THREE.PlaneGeometry(100, 100, 1, 1);
 
@@ -37,20 +37,15 @@ export default class InfiniteGridHelper extends THREE.Mesh {
       },
       transparent: true,
       vertexShader: `
-         
          varying vec3 worldPosition;
-     
          uniform float uDistance;
          
          void main() {
-         
               vec3 pos = position.${axes} * uDistance;
               pos.${planeAxes} += cameraPosition.${planeAxes};
               
               worldPosition = pos;
-              
               gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
-         
          }
          `,
 
