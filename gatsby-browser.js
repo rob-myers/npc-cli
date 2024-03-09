f1ea3da15e5cececd83cf32ec98fba852878d7e3
Rob Myers <me.robmyers@gmail.com>
2024-03-09 10:39:36 +0000

    feat: react-query listens for focus in dev

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,20 @@ import "flexlayout-react/style/light.css";
 import "xterm/css/xterm.css";
 
 export { wrapPageElement } from "./src/components/Root";
+
+import { focusManager } from "@tanstack/react-query";
+
+/**
+ * In development refetch on refocus can automate changes.
+ * In production, see https://github.com/TanStack/query/pull/4805.
+ */
+if (process.env.NODE_ENV !== "production") {
+  focusManager.setEventListener((handleFocus) => {
+    if (typeof window !== "undefined" && window.addEventListener) {
+      window.addEventListener("focus", handleFocus, false);
+    }
+    return () => {
+      window.removeEventListener("focus", handleFocus);
+    };
+  });
+}
