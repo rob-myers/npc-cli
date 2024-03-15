63283905655fa292402f10f96191c4b724891898
robmyers <me.robmyers@gmail.com>
2024-03-15 10:11:24 +0000

    feat: more visible Origin

--- src/npc-cli/aux/MiscThree.jsx
+++ src/npc-cli/aux/MiscThree.jsx
@@ -5,7 +5,7 @@ import React from "react";
  */
 export function Origin() {
   return (
-    <mesh scale={[0.025, 0, 0.025]} position={[0, 0.0001, 0]}>
+    <mesh scale={[0.025, 1, 0.025]} position={[0, 0.5 - 0.001, 0]}>
       <boxGeometry args={[1, 1, 1]} />
       <meshBasicMaterial color="red" />
     </mesh>
