831e826473aa0c4f4a911f400b13f92402e43aea
robmyers <me.robmyers@gmail.com>
2024-03-31 18:29:39 +0100

    feat: agents have direction + smoother turn

--- src/npc-cli/aux/TestNpcs.jsx
+++ src/npc-cli/aux/TestNpcs.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import * as THREE from "three";
+import { dampLookAt, dampAngle } from "maath/easing";
 
 import { wallOutset, worldScale } from "../service/const";
 import { info } from "../service/generic";
@@ -12,28 +13,30 @@ import useStateRef from "../hooks/use-state-ref";
 export default function TestNpcs(props) {
   const api = React.useContext(TestWorldContext);
 
-  // prettier-ignore
   const state = useStateRef(/** @returns {State} */ () => ({
     selected: 0,
     toAgent: {},
-    toMesh: {}, // 🚧 on remove agent need to update this
-    moveMesh(agent, mesh) {
+    toGroup: {}, // 🚧 on remove agent need to update this
+    moveGroup(agent, mesh) {
       const position = agent.position();
       mesh.position.set(position.x, position.y + agentHeight/2, position.z);
 
-      // 🚧 lerp angle
-      const velocity = agent.velocity();
-      mesh.lookAt(tmpVector3.copy(mesh.position).add(velocity));
+      const velocity = tmpV3_1.copy(agent.velocity());
+      if (velocity.length() > 0.2) {
+        dampLookAt(mesh, tmpV3_2.copy(mesh.position).add(velocity), 0.25, api.timer.getDelta());
+      }
     },
     update() {
       for (const agent of api.crowd.getAgents()) {
-        const mesh = state.toMesh[agent.agentIndex];
-        state.moveMesh(agent, mesh);
+        const mesh = state.toGroup[agent.agentIndex];
+        state.moveGroup(agent, mesh);
       }
     },
     updateAgentColor(agentId) {
-      const mesh = state.toMesh[agentId];
-      /** @type {THREE.MeshBasicMaterial} */ (mesh.material).color = state.selected === agentId ? greenColor : redColor
+      const mesh = state.toGroup[agentId].children[0];
+      if (mesh instanceof THREE.Mesh && mesh.material instanceof THREE.MeshBasicMaterial) {
+        mesh.material.color = state.selected === agentId ? greenColor : redColor
+      }
     },
   }));
 
@@ -50,25 +53,32 @@ export default function TestNpcs(props) {
   }, [api.disabled, props.crowd]);
 
   return Object.values(state.toAgent).map((agent) => (
-    <mesh
+    <group
       key={agent.agentIndex}
-      onUpdate={(mesh) => {
-        state.toMesh[agent.agentIndex] = mesh;
-        state.moveMesh(agent, mesh);
-        state.updateAgentColor(Number(agent.agentIndex));
-      }}
-      onPointerUp={(e) => {
-        info("clicked npc", agent.agentIndex);
-        state.selected = agent.agentIndex;
-        Object.keys(state.toMesh).forEach((agentIdStr) =>
-          state.updateAgentColor(Number(agentIdStr))
-        );
-        e.stopPropagation();
+      ref={(group) => {
+        if (group) {
+          state.toGroup[agent.agentIndex] = group;
+          state.moveGroup(agent, group);
+          state.updateAgentColor(Number(agent.agentIndex));
+        }
       }}
     >
-      <meshBasicMaterial />
-      <cylinderGeometry args={[agentRadius, agentRadius, agentHeight]} />
-    </mesh>
+      <mesh
+        onPointerUp={(e) => {
+          info("clicked npc", agent.agentIndex);
+          state.selected = agent.agentIndex;
+          Object.keys(state.toGroup).forEach((agentIdStr) =>
+            state.updateAgentColor(Number(agentIdStr))
+          );
+          e.stopPropagation();
+        }}
+      >
+        <meshBasicMaterial />
+        <cylinderGeometry args={[agentRadius, agentRadius, agentHeight]} />
+        {/* <capsuleGeometry args={[agentRadius, agentHeight / 2]} /> */}
+      </mesh>
+      <arrowHelper args={[tmpV3_unitZ, undefined, 0.7, "blue", undefined, 0.1]} />
+    </group>
   ));
 }
 
@@ -82,14 +92,17 @@ export default function TestNpcs(props) {
  * @typedef State
  * @property {number} selected
  * @property {Record<string, NPC.CrowdAgent>} toAgent
- * @property {Record<string, THREE.Mesh>} toMesh
+ * @property {Record<string, THREE.Group>} toGroup
  * @property {(agentId: number) => void} updateAgentColor
- * @property {(agent: NPC.CrowdAgent, mesh: THREE.Mesh) => void} moveMesh
+ * @property {(agent: NPC.CrowdAgent, mesh: THREE.Group) => void} moveGroup
  * @property {() => void} update
  */
 
 const agentRadius = wallOutset * worldScale;
 const agentHeight = 1.5;
-const tmpVector3 = new THREE.Vector3();
+const tmpV3_1 = new THREE.Vector3();
+const tmpV3_2 = new THREE.Vector3();
+const tmpV3_unitX = new THREE.Vector3(1, 0, 0);
+const tmpV3_unitZ = new THREE.Vector3(0, 0, 1);
 const redColor = new THREE.Color("red");
 const greenColor = new THREE.Color("green");
