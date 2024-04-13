b45e303421ed858815e1fab86a2175f309cc9d87
Rob Myers <me.robmyers@gmail.com>
2024-04-13 10:47:17 +0100

    feat: images script converts png to webp

--- src/scripts/images.js
+++ src/scripts/images.js
@@ -4,20 +4,20 @@ import path from 'path';
 import { createCanvas, loadImage } from 'canvas';
 
 import { ASSETS_JSON_FILENAME, GEOMORPHS_JSON_FILENAME } from './const';
-import { saveCanvasAsFile } from './service';
-import { worldScale } from '../npc-cli/service/const'; // sucrase-node needs relative paths
+import { runYarnScript, saveCanvasAsFile } from './service';
+// sucrase-node needs relative paths
+import { worldScale } from '../npc-cli/service/const';
 import { geomorphService } from '../npc-cli/service/geomorph';
 
 const staticAssetsDir = path.resolve(__dirname, "../../static/assets");
 const mediaDir = path.resolve(__dirname, "../../media");
 const symbolsDir = path.resolve(mediaDir, "symbol");
 const mapsDir = path.resolve(mediaDir, "map");
-const outputDir = path.resolve(staticAssetsDir, "2d");
+const assets2dDir = path.resolve(staticAssetsDir, "2d");
 
 (async function main() {
-
   // ensure output directory
-  fs.mkdirSync(outputDir, { recursive: true });
+  fs.mkdirSync(assets2dDir, { recursive: true });
 
   const assets = geomorphService.deserializeAssets(
     JSON.parse(fs.readFileSync(path.resolve(staticAssetsDir, ASSETS_JSON_FILENAME)).toString())
@@ -26,19 +26,19 @@ const outputDir = path.resolve(staticAssetsDir, "2d");
     JSON.parse(fs.readFileSync(path.resolve(staticAssetsDir, GEOMORPHS_JSON_FILENAME)).toString())
   );
 
-  // 🚧 png + webp
   const canvas = createCanvas(0, 0);
   const ct = canvas.getContext('2d');
 
   const layouts = Object.values(geomorphs.layout);
+  const pngToProm = /** @type {{ [pngFilename: string]: Promise<any> }} */ ({});
 
   for (const { key: gmKey, pngRect } of layouts) {
-
     // e.g. 1.5m --> 60sgu (Starship Geomorph Units)
     pngRect.scale(1 / worldScale);
 
     canvas.width = pngRect.width;
     canvas.height = pngRect.height;
+    // 🚧
     ct.fillStyle = "red";
     ct.fillRect(0, 0, canvas.width, canvas.height);
     ct.fill();
@@ -46,8 +46,11 @@ const outputDir = path.resolve(staticAssetsDir, "2d");
     const debugImg = await loadImage(fs.readFileSync(path.resolve(staticAssetsDir, 'debug', `${gmKey}.png`)))
     ct.drawImage(debugImg, 0, 0);
 
-    saveCanvasAsFile(canvas, path.resolve(outputDir, `${gmKey}.floor.png`));
+    const pngPath = path.resolve(assets2dDir, `${gmKey}.floor.png`);
+    pngToProm[pngPath] = saveCanvasAsFile(canvas, pngPath);
   }
 
+  await Promise.all(Object.values(pngToProm));
+  await runYarnScript('cwebp-fast', JSON.stringify({ files: Object.keys(pngToProm) }), '--webp=50', '--quality=90');
 
 })();
--- src/scripts/service.js
+++ src/scripts/service.js
@@ -1,5 +1,7 @@
 import fs from 'fs';
+import childProcess from 'child_process';
 import stream from 'stream/promises';
+import { info } from '../npc-cli/service/generic';
 
 /**
  * @param {import('canvas').Canvas} canvas 
@@ -10,4 +12,25 @@ export async function saveCanvasAsFile(canvas, outputPath) {
     canvas.createPNGStream(), 
     fs.createWriteStream(outputPath),
   );
+}
+
+/**
+ * Options can be provided as single args like `--quality=75`.
+ * @param {string} scriptName
+ * @param {string[]} args
+ */
+export async function runYarnScript(scriptName, ...args) {
+  await /** @type {Promise<void>} */ (new Promise((resolve, reject) => {
+      const proc = childProcess.spawn('yarn', [scriptName, ...args]);
+      proc.stdout.on('data', (data) => info(scriptName, data.toString()));
+      // stderr needn't contain error messages
+      proc.stderr.on('data', (data) => info(scriptName, data.toString()));
+      proc.stdout.on('close', () => resolve());
+      proc.on('error', (e) => reject(e));
+      proc.on('exit', (errorCode) => {
+        if (typeof errorCode === 'number' && errorCode !== 0) {
+          reject({ errorCode });
+        }
+      });
+  }));
 }
