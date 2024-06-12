36eeec8592cb844022ad29f390894f586d748f3b
Rob Myers <me.robmyers@gmail.com>
2024-06-13 00:21:36 +0100

    feat: try brighter skin via gl.toneMapping

--- src/npc-cli/aux/TestWorldCanvas.jsx
+++ src/npc-cli/aux/TestWorldCanvas.jsx
@@ -213,7 +213,7 @@ export default function TestWorldCanvas(props) {
       className={canvasCss}
       frameloop={props.disabled ? "demand" : "always"}
       resize={{ debounce: 300 }}
-      gl={{ toneMapping: 4, toneMappingExposure: 1, logarithmicDepthBuffer: true }}
+      gl={{ toneMapping: 4, toneMappingExposure: 3, logarithmicDepthBuffer: true }}
       onCreated={state.onCreated}
       onPointerDown={state.onPointerDown}
       onPointerMissed={state.onPointerMissed}
