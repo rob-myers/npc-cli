722f53f58c963aace8acc5728660301b3c863490
Rob Myers <me.robmyers@gmail.com>
2024-06-21 23:01:57 +0100

    feat: extra npc in demo scene

--- src/npc-cli/sh/src/profile-1.sh
+++ src/npc-cli/sh/src/profile-1.sh
@@ -3,6 +3,7 @@ awaitWorld
 setupDemo1
 
 api npc.spawn '{ npcKey: "kate", point: { x: 5 * 1.5, y: 0, z: 7 * 1.5 }, agent: true }' >/dev/null
+api npc.spawn '{ npcKey: "will", point: { x: 2.5, y: 0, z: 3 * 1.5 }, agent: true }' >/dev/null
 api npc.spawn '{ npcKey: "rob", point: { x: 1 * 1.5, y: 0, z: 5 * 1.5 }, agent: true }'  >/dev/null
 
 api '({ npc }) => { npc.select.curr = "rob" }'
