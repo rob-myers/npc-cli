89113a91958315ace86e7a335f66f875db16223a
robmyers <me.robmyers@gmail.com>
2024-07-08 19:16:57 +0100

    feat: back to greyscale ceil + obs

--- src/npc-cli/world/Obstacles.jsx
+++ src/npc-cli/world/Obstacles.jsx
@@ -162,7 +162,7 @@ export default function Obstacles(props) {
         side={THREE.DoubleSide}
         transparent
         map={w.obsTex}
-        diffuse={new THREE.Vector3(0.5, 0.5, 0.7)}
+        diffuse={new THREE.Vector3(0.5, 0.5, 0.5)}
       />
     </instancedMesh>
   );
