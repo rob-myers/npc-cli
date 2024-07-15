e8231f372a956f0c51b075d0d97ac9fdd10d45aa
robmyers <me.robmyers@gmail.com>
2024-07-15 13:20:48 +0100

    feat: adjust card style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,12 +14,14 @@ export default function Card(props) {
 }
 
 const rootCss = css`
+  margin: 32px 0;
   padding: 16px 48px 32px 48px;
   background-color: #eee;
   border-left: 4px solid #ddd;
   font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
   
   @media(max-width:${breakpoint}) {
+    margin: 16px 0 24px;
     padding: 8px 32px 20px 32px;
   }
 `;
