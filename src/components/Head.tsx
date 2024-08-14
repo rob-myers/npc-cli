83a0aad8900448bb30ba3401c1dd6ca111899c40
Rob Myers <me.robmyers@gmail.com>
2024-08-14 14:16:20 +0100

    feat: try fix mobile tty hidden on expand keyboard

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -4,6 +4,12 @@ export function Head() {
   return (
     <>
       <title>NPC CLI</title>
+      <meta
+          // Shrink on open mobile keyboard
+          // https://www.reddit.com/r/webdev/comments/195vkgu/comment/kht2py0
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, interactive-widget=resizes-content"
+        />
     </>
   );
 }
