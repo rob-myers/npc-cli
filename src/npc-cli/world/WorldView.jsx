ef13c56d4ee040d38a68019ee0b4d1fd11c76658
Rob Myers <me.robmyers@gmail.com>
2024-11-14 21:53:00 +0000

    feat: delta

--- src/npc-cli/world/WorldView.jsx
+++ src/npc-cli/world/WorldView.jsx
@@ -370,7 +370,7 @@ export default function WorldCanvas(props) {
       }
 
       <PerspectiveCamera
-        position={[0, 64, 0]}
+        position={[0, 32, 0]}
         makeDefault
         fov={state.fov}
         zoom={1}
