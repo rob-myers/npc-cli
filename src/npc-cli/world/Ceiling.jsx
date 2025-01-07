7fa07abe486d0d935cebf823fc90c17f5c3cf6e7
Rob Myers <me.robmyers@gmail.com>
2025-01-07 19:09:43 +0000

    feat: lighter Ceiling outline

--- src/npc-cli/world/Ceiling.jsx
+++ src/npc-cli/world/Ceiling.jsx
@@ -47,7 +47,7 @@ export default function Ceiling(props) {
       // wall/door tops
       const black = 'black';
       const grey90 = 'rgb(90, 90, 90)';
-      const wallsColor = '#666';
+      const wallsColor = '#888';
       const hullDoorsColor = '#777';
       const grey100 = 'rgb(100, 100, 100)';
       const thinLineWidth = 0.04;
