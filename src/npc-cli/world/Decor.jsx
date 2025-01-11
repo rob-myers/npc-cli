bf7173f219017ff5c628a53e52177ffb26b2a7ba
Rob Myers <me.robmyers@gmail.com>
2025-01-11 09:32:12 +0000

    feat: fiddle with opacity/diffuse

--- src/npc-cli/world/Decor.jsx
+++ src/npc-cli/world/Decor.jsx
@@ -574,7 +574,7 @@ export default function Decor(props) {
       {ready && <instancedMultiTextureMaterial
         key={glsl.InstancedMultiTextureMaterial.key}
         alphaTest={0.5}
-        diffuse={[0.5, 0.5, 0.5]}
+        diffuse={[0.7, 0.7, 0.7]}
         atlas={w.texDecor.tex}
         objectPickRed={5}
         opacity={query.status === 'success' ? 1 : 0}
