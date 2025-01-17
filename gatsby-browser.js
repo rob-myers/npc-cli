bfca03f45a18f01f9a48ee0b98fdf6bc07b8bb41
robmyers <me.robmyers@gmail.com>
2025-01-17 15:21:42 +0000

    feat: fix hmr Tabs flicker again

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,5 @@
+import "./src/components/globals.css";
+import "@xterm/xterm/css/xterm.css";
+// import "flexlayout-react/style/light.css"; // 🔔 see Head.tsx
+
 export { wrapPageElement } from "./src/components/Page";
