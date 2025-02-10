3cc077b73e991847b55fdb034b9275289a1eeeff
Rob Myers <me.robmyers@gmail.com>
2025-02-10 21:26:39 +0000

    feat: adjust fov

--- src/npc-cli/sh/src/profile-1.sh
+++ src/npc-cli/sh/src/profile-1.sh
@@ -44,7 +44,7 @@ ptags=no-pause; click | filter meta.floor | map --forever '(input, { w, home }) 
 }' &
 
 w update 'w => w.decor.showLabels = true'
-w update 'w => w.view.targetFov = 20'
+w update 'w => w.view.targetFov = w.smallViewport ? 10 : 15'
 
 setupContextMenu
 ptags=no-pause; events | handleContextMenu &
